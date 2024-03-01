e527c33e0b087dcbaf4b3c2f4482d75098012f30
Dinesh Priyantha <dineshpriyantha200248@gmail.com>
2024-03-01 14:49:06 +0530

    JavaVariable JavaVariExample

---
+++ src/VariableExample.java
@@ -0,0 +1,11 @@
+public class VariableExample {
+    public static void main(String[] args){
+
+        double length= 5.5;
+        double widht=3.0;
+
+        double area= length*widht;
+
+        System.out.println(area);
+    }
+}
